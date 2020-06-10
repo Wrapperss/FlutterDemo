e45fee7e0e4213b6e62a13a71e3912fa588c6fc3
Wrappers <zhanglive@outlook.com>
2020-06-11 07:26:06 +0800

    feat: code at 2020.6.10

---
+++ FirstApp/lib/RouterTestRoute.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class RouterTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+    // return Center(child: RaisedButton(onPressed: ,),)
+  }
+}
