e5e67b4194b2ad2e2ac055d1c1b5504bfbddec6c
Wrapperss <zhanglive@outlook.com>
2020-07-08 14:35:20 +0800

    feat: code at 2020.7.8

--- FirstApp/lib/FunctionComponents/Animation/AnimationRoute.dart
+++ FirstApp/lib/FunctionComponents/Animation/AnimationRoute.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'FadeRoute.dart';
+import 'HeroAnimationRout.dart';
+import 'ScaleAnimationRoute.dart';
+import 'StaggerRoute.dart';
 
 class AnimationRoute extends StatelessWidget {
   @override
@@ -8,8 +12,63 @@ class AnimationRoute extends StatelessWidget {
         title: Text("Animation"),
       ),
       body: Center(
-        child: Text("Animation"),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Scale"),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ScaleAnimationRoute(),
+                    ));
+              },
+            ),
+            RaisedButton(
+                child: Text("Fade"),
+                onPressed: () =>
+                    Navigator.push(context, FadeRoute(builder: (context) {
+                      return ScaleAnimationRoute();
+                    }))),
+            InkWell(
+              child: Hero(
+                tag: "avatar",
+                child: ClipOval(
+                  child: Image.asset(
+                    "images/avatar.jpeg",
+                    width: 50,
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context, PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: Scaffold(
+                        appBar: AppBar(
+                          title: Text("原图"),
+                        ),
+                        body: HeroAnimationRoute(),
+                      ),
+                    );
+                  },
+                ));
+              },
+            ),
+            RaisedButton(
+              child: Text("Stagger"),
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => StaggerRoute(),
+                  )),
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+class HeroAnimationRout {}
---
+++ FirstApp/lib/FunctionComponents/Animation/ScaleAnimationRoute.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class ScaleAnimationRoute extends StatefulWidget {
+  @override
+  _ScaleAnimationRouteState createState() => _ScaleAnimationRouteState();
+}
+
+//需要继承TickerProvider，如果有多个AnimationController，则应该使用TickerProviderStateMixin。
+class _ScaleAnimationRouteState extends State<ScaleAnimationRoute>
+    with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
+
+    animation = Tween(begin: 0.0, end: 300.0).animate(controller)
+      ..addListener(() {
+        setState(() => {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          //动画执行结束时反向执行动画
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          //动画恢复到初始状态时执行动画（正向）
+          controller.forward();
+        }
+      });
+
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scale"),
+      ),
+      // body: Center(
+      //   child: Image.asset(
+      //     "images/avatar.jpeg",
+      //     width: animation.value,
+      //     height: animation.value,
+      //   ),
+      // ),
+      body: Center(
+        child: AnimatedBuilder(
+          child: Image.asset(
+            "images/avatar.jpeg",
+          ),
+          animation: animation,
+          builder: (context, child) {
+            return Container(
+              height: animation.value,
+              width: animation.value,
+              child: child,
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    //路由销毁时需要释放动画资源
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatedImage extends AnimatedWidget {
+  AnimatedImage({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+
+    return Center(
+      child: Image.asset("images/avatar.png",
+          width: animation.value, height: animation.value),
+    );
+  }
+}
