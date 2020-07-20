bf3f42fb9f005b5943729bd4ce2405e2d1432b86
Wrapperss <zhanglive@outlook.com>
2020-07-20 16:34:02 +0800

    feat: code at 2020.7.20

--- github_client_app/lib/main.dart
+++ github_client_app/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:github_client_app/common/global.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => Global.init().then((e) => runApp(MyApp()));
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
